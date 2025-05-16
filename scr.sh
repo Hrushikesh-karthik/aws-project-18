0e642ce61e9ec3ffd189d782effcb7f2c32437d7
U Hrushikesh Karthik <143626493+Hrushikesh-karthik@users.noreply.github.com>
2025-05-16 23:20:50 +0530

    Update scr.sh

--- scr.sh
+++ scr.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y python3 git
+sudo apt update -y
+sudo apt install -y python3 git
 git clone https://github.com/Hrushikesh-karthik/aws-project-18.git
 cd aws-project-18
+sudo apt install -y python3-venv
+python3 -m venv myenv
+source myenv/bin/activate
+
 pip3 install -r requirements.txt
